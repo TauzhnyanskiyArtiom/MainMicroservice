d9611fd2f802f9bb085961238483749c416b3d25
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2022-02-19 15:29:33 +0200

    Add unit and integration tests classes for UserServiceImpl

---
+++ src/test/java/com/onpu/web/integration/service/implementation/UserServiceImplIT.java
@@ -0,0 +1,12 @@
+package com.onpu.web.integration.service.implementation;
+
+import com.onpu.web.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+class UserServiceImplIT {
+
+}
