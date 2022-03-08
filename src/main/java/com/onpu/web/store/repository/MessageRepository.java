2c1fef5b48249cb5e80f416dc84f53bf91df028c
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2022-03-08 17:12:22 +0200

    Add UserRevisionListener and new columns in RevisionEntity: userId, username

--- src/main/java/com/onpu/web/store/repository/MessageRepository.java
+++ src/main/java/com/onpu/web/store/repository/MessageRepository.java
@@ -1,15 +1,19 @@
 package com.onpu.web.store.repository;
 
+import com.onpu.web.store.entity.CommentEntity;
 import com.onpu.web.store.entity.MessageEntity;
 import com.onpu.web.store.entity.UserEntity;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+public interface MessageRepository extends
+        JpaRepository<MessageEntity, Long>,
+        RevisionRepository<MessageEntity, Long, Long> {
 
     @Override
     @EntityGraph(attributePaths = { "comments", "author"})
